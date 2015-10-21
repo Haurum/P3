c6e44e624582658ee6d69c1a1bb6536ffa0469d1
Lyngse <superlyng@msn.com>
2015-10-21 16:19:53 +0200

    Lort :+1:

--- Prototype/js/custom/app.js
+++ Prototype/js/custom/app.js
@@ -18,6 +18,10 @@ angular.module('tournyplanner', ['ngRoute'])
       templateUrl: 'templates/tournament-divs-detail.html',
       controller: 'DivisonDetailController'
     }).
+      when('/addFields', {
+      templateUrl: 'templates/baner.html',
+      controller: 'CreateFieldController'
+    }).
     otherwise({
       redirectTo: '/'
     });
@@ -27,11 +31,17 @@ angular.module('tournyplanner', ['ngRoute'])
   $rootScope.divisions = ["Noobs", "Gorrilaer", "Gulleroderne", "Master", "Challenger"];
 })
 
+.run(function($rootScope) {
+  $rootScope.fields = ["Bane 7A","Bane 7B","Bane 7C","Bane 7D"];
+})
+
 .controller('HomeController', ['$scope', function ($scope) {
 }])
 
 .controller('CreateTournyController', ['$scope', function ($scope) {
-  console.log("bhisrhbs");
+}])
+
+.controller('CreateFieldController', ['$scope', function ($scope){
 }])
 
 .controller('DivisonController', ['$scope', '$rootScope', '$location', function ($scope, $rootScope, $location) {
@@ -39,6 +49,8 @@ angular.module('tournyplanner', ['ngRoute'])
   $scope.newDivName = "";
   
   $scope.new = false;
+
+  $scope.newFieldName = "";
   
   $scope.createNew = function() {
     $scope.new = !$scope.new;
@@ -48,9 +60,23 @@ angular.module('tournyplanner', ['ngRoute'])
     $scope.newDivName = "";
     $scope.createNew();
   }
+
+  $scope.submitField = function(newName) {
+    $rootScope.fields.push(newName);
+    $scope.newFieldName = "";
+    $scope.createNew();
+  }
   
   $scope.remove = function(index) {
     $rootScope.divisions.splice(index, 1);
+  }
+
+  $scope.removeField = function(index) {
+    $rootScope.fields.splice(index, 1);
+  }
+
+  $scope.addField = function(index) {
+    $rootScope.fields.spilce(index + 1);
   }
   
   $scope.gotoDivison = function(currDiv) {
