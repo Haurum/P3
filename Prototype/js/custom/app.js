948032b8a5a252811643763279ce6d5413be25b6
Lyngse <superlyng@msn.com>
2015-10-22 15:52:09 +0200

    Prototype

--- Prototype/js/custom/app.js
+++ Prototype/js/custom/app.js
@@ -1,4 +1,4 @@
-angular.module('tournyplanner', ['ngRoute'])
+angular.module('tournyplanner', ['ngRoute', 'ui.bootstrap'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.
@@ -21,7 +21,23 @@ angular.module('tournyplanner', ['ngRoute'])
     when('/addFields', {
       templateUrl: 'templates/baner.html',
       controller: 'CreateFieldsController'
-    }).    
+    }).
+    when('/DivisionMatchOverview', {
+      templateUrl: 'templates/DivisionMatchOverview.html',
+      controller: 'DivisionMatchOverviewController'
+    }).
+    when('/TeamMatchOverview', {
+      templateUrl: 'templates/TeamMatchOverview.html',
+      controller: 'TeamMatchOverviewController'
+    }).
+    when('/PoolMatchOverview', {
+      templateUrl: 'templates/PoolMatchOverview.html',
+      controller: 'PoolMatchOverviewController'
+    }).
+    when('/TournamentSchedule', {
+      templateUrl: 'templates/TournamentSchedule.html',
+      controller: 'TournamentScheduleController'
+    }).     
     otherwise({
       redirectTo: '/'
     });
@@ -39,6 +55,23 @@ angular.module('tournyplanner', ['ngRoute'])
 
 .controller('CreateTournyController', ['$scope', function ($scope) {
 
+}])
+
+.controller('DivisionMatchOverviewController', ['$scope', function ($scope) {
+  $scope.isCollapsed = true;
+}])
+
+.controller('PoolMatchOverviewController', ['$scope', function ($scope) {
+  $scope.isCollapsed = true;
+}])
+
+.controller('TeamMatchOverviewController', ['$scope', function ($scope) {
+  $scope.isCollapsed = true;
+}])
+
+
+.controller('TournamentScheduleController', ['$scope', function ($scope) {
+  $scope.isCollapsed = true;
 }])
 
 .controller('CreateFieldsController', ['$scope', '$rootScope', function ($scope, $rootScope) {
@@ -93,8 +126,6 @@ angular.module('tournyplanner', ['ngRoute'])
     $rootScope.FmFields.splice(index, 1);
   }  
 
-
-
 }])
 
 .controller('DivisonController', ['$scope', '$rootScope', '$location', function ($scope, $rootScope, $location) {
@@ -126,3 +157,80 @@ angular.module('tournyplanner', ['ngRoute'])
   $scope.division = $rootScope.currDivision;
 }])
 
+/* DATE PICKER START! */
+
+.controller('DatepickerDemoCtrl', function ($scope) {
+  $scope.today = function() {
+    $scope.dt = new Date();
+  };
+  $scope.today();
+
+  $scope.clear = function () {
+    $scope.dt = null;
+  };
+
+  // Disable weekend selection
+  $scope.disabled = function(date, mode) {
+    return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+  };
+
+  $scope.toggleMin = function() {
+    $scope.minDate = $scope.minDate ? null : new Date();
+  };
+  $scope.toggleMin();
+  $scope.maxDate = new Date(2020, 5, 22);
+
+  $scope.open = function($event) {
+    $scope.status.opened = true;
+  };
+
+  $scope.setDate = function(year, month, day) {
+    $scope.dt = new Date(year, month, day);
+  };
+
+  $scope.dateOptions = {
+    formatYear: 'yy',
+    startingDay: 1
+  };
+
+  $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+  $scope.format = $scope.formats[0];
+
+  $scope.status = {
+    opened: false
+  };
+
+  var tomorrow = new Date();
+  tomorrow.setDate(tomorrow.getDate() + 1);
+  var afterTomorrow = new Date();
+  afterTomorrow.setDate(tomorrow.getDate() + 2);
+  $scope.events =
+    [
+      {
+        date: tomorrow,
+        status: 'full'
+      },
+      {
+        date: afterTomorrow,
+        status: 'partially'
+      }
+    ];
+
+  $scope.getDayClass = function(date, mode) {
+    if (mode === 'day') {
+      var dayToCheck = new Date(date).setHours(0,0,0,0);
+
+      for (var i=0;i<$scope.events.length;i++){
+        var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
+
+        if (dayToCheck === currentDay) {
+          return $scope.events[i].status;
+        }
+      }
+    }
+
+    return '';
+  };
+});
+
+/* DATE PICKER END! */ 
