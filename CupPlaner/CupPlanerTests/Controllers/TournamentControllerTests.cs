8ea28f8eafffe45518971eca0670163a43c68b61
bkhoja <bilalkhwaja2694@gmail.com>
2015-11-11 14:09:08 +0100

    files added to project

--- CupPlaner/CupPlanerTests/Controllers/TournamentControllerTests.cs
+++ CupPlaner/CupPlanerTests/Controllers/TournamentControllerTests.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CupPlaner.Controllers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CupPlanerTests.Controllers
+namespace CupPlaner.Controllers.Tests
 {
-    class TournamentControllerTests
+    [TestClass()]
+    public class TournamentControllerTests
     {
+        [TestMethod()]
+        public void DetailsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateTest()
+        {
+            Assert.Fail();
+        }
     }
 }
