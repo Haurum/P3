148aaf0c91fc5b731a0a4d3c530830548f62f390
bovle <frederik.bl@live.dk>
2015-11-12 15:38:48 +0100

    add tournament næsten god!

--- Prototype/js/custom/tournament.js
+++ Prototype/js/custom/tournament.js
@@ -1,35 +1,33 @@
 app.controller('TournamentController', ['$scope', '$rootScope', '$location', '$http', '$routeParams', function ($scope, $rootScope, $location, $http, $routeParams) {
   $rootScope.Tournament.password = "";
 
-  $scope.getDivisions = function(){
-    $http.get("http://localhost:50229/Tournament/Details" +  $routeParams.tournamentId)
-      .success(function(data)
-      {
+  $scope.getDivisions = function () {
+    $http.get("http://localhost:50229/Tournament/Details" + $routeParams.tournamentId)
+      .success(function (data) {
         $scope.divisions = data.Divisions;
-      }).error(function(err) 
-      {
+      }).error(function (err) {
         $scope.error = err;
-    })
+      })
   }
 
   $scope.getDivisions();
-  
+
   $scope.newDivName = "";
-  
+
   $scope.new = false;
-  
-  $scope.createNew = function() {
+
+  $scope.createNew = function () {
     $scope.new = !$scope.new;
   }
-  $scope.submit = function(newName) {
-    $rootScope.divisions.push({ MatchDuration: "30", FieldSize: "11-mands", Name: newName, Pool: []});
+  $scope.submit = function (newName) {
+    $rootScope.divisions.push({ MatchDuration: "30", FieldSize: "11-mands", Name: newName, Pool: [] });
     $scope.newDivName = "";
     $scope.createNew();
   }
-  
-  $scope.gotoDivison = function(currDiv, index) {
+
+  $scope.gotoDivison = function (currDiv, index) {
     $rootScope.currDivisionIndex = index;
-    $location.url("tournament/" + $routeParams.tournamentId+ "/division/" + currDiv.Id);
+    $location.url("tournament/" + $routeParams.tournamentId + "/division/" + currDiv.Id);
   }
 }]);
 
@@ -40,44 +38,44 @@ app.controller('CreateTournyController', ['$scope', '$rootScope', '$http', '$loc
   
   /* DATE PICKER START */
   $scope.dateRange = 0;
-  $scope.today = function() {
+  $scope.today = function () {
     $scope.startDate = new Date();
     $scope.startDate.setHours(0);
     $scope.startDate.setSeconds(0);
     $scope.startDate.setMinutes(0);
     $scope.startDate.setMilliseconds(0);
-    $scope.endDate = new Date(); 
+    $scope.endDate = new Date();
     $scope.endDate.setHours(0);
     $scope.endDate.setSeconds(0);
     $scope.endDate.setMinutes(0);
     $scope.endDate.setMilliseconds(0);
   };
   $scope.today();
-  
+
   $scope.dateArray = [$scope.startDate];
-  
-  $scope.toggleMin = function() {
+  $scope.startTimes = [];
+  $scope.endTimes = [];
+
+  $scope.toggleMin = function () {
     $scope.minDate = $scope.minDate ? null : new Date();
   };
   $scope.toggleMin();
   $scope.maxDate = new Date(2020, 5, 22);
-  $scope.openEndDate = function($event) {
+  $scope.openEndDate = function ($event) {
     $scope.statusEndDate.opened = true;
   };
-  
-  $scope.isChanged = function()
-  {
-    $scope.dateRange = ($scope.endDate-$scope.startDate)/(1000*60*60*24);
+
+  $scope.isChanged = function () {
+    $scope.dateRange = ($scope.endDate - $scope.startDate) / (1000 * 60 * 60 * 24);
     $scope.dateArray = [];
-    for (var i = 0; i <= $scope.dateRange; i++)
-    {
+    for (var i = 0; i <= $scope.dateRange; i++) {
       var date = new Date($scope.startDate.getTime());
       date.setDate(date.getDate() + i);
       $scope.dateArray.push(date);
     }
   }
-  
-  $scope.openStartDate = function($event) {
+
+  $scope.openStartDate = function ($event) {
     $scope.statusStartDate.opened = true;
   };
   $scope.dateOptions = {
@@ -92,34 +90,53 @@ app.controller('CreateTournyController', ['$scope', '$rootScope', '$http', '$loc
   $scope.statusEndDate = {
     opened: false
   };
-  /* DATE PICKER END */ 
-  
-  
-  
+  /* DATE PICKER END */
 
-  $scope.uploadTournament = function()
-  {
-    var tournamentData = {
-      tournamentName: $scope.tournamentName,
-      tournamentPassword: $scope.tournamentPassword,
-      tournamentStartDate: $scope.startDate,
-      tournamentEndDate: $scope.endDate
-    }
 
-    $http.post("http://localhost:50229/addTournament/", tournamentData).success(function(tournamentData)
+
+
+
+  $scope.uploadTournament = function () {
+    if ($scope.tournamentName && $scope.tournamentPassword)
     {
-      if (tournamentData.tournamentName != 0 && tournamentData.password != 0 && tournamentData.tournamentStartDate != 0 && tournamentData.tournamentEndDate != 0)
-      {
-        $scope.error = false;
-        $location.path("tournament/1");
+      $scope.startDateTimes = [];
+      $scope.endDateTimes = [];
+      $scope.error = false;
+      for (var index = 0; index < $scope.startTimes.length; index++) {
+        var startDateTime = new Date($scope.dateArray[index].getTime() + $scope.startTimes[index].getHours() * 60 * 60 * 1000 + $scope.startTimes[index].getMinutes() * 60 * 1000);
+        $scope.startDateTimes[index] = startDateTime.toISOString();
+        
+        var endDateTime = new Date($scope.dateArray[index].getTime() + $scope.endTimes[index].getHours() * 60 * 60 * 1000 + $scope.endTimes[index].getMinutes() * 60 * 1000);
+        $scope.endDateTimes[index] = endDateTime.toISOString();
+        
+        if($scope.startDateTimes[index] === {} && $scope.endDateTimes[index] === null){
+          $scope.error = "en start eller slut tid er ikke sat";
+          break;
+        }
+        
       }
-      else{
-        $scope.error = true;
-      }
-    }).error(function(err) 
-    {
-      $scope.error = err;
-    });
+      if(!$scope.error){
+        var tournamentData = {
+          name: $scope.tournamentName,
+          password: $scope.tournamentPassword,
+          startTimes: $scope.startDateTimes,
+          endTimes: $scope.endDateTimes
+        }
+    
+        $http.post("http://localhost:50229/Tournament/Create/", tournamentData).success(function(Data)
+        {
+          if(Data.status === "error"){
+            $scope.error = Data.message;
+          }
+        }).error(function(err) 
+        {
+          $scope.error = "kunne ikke uploade til serveren";
+        });
+      }    
+    }else{
+      $scope.error = "navn eller kode ikke sat";
+    }   
+    
   }
 
 }]);
