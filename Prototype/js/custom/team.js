1961c1f4c73f98d2aea3eb4637b2fcb23c654ec5
Lyngse <superlyng@msn.com>
2015-11-18 15:54:34 +0100

    Fuuuuck

--- Prototype/js/custom/team.js
+++ Prototype/js/custom/team.js
@@ -7,38 +7,7 @@ app.controller('TeamDetailController', ['$scope', '$rootScope', '$location', '$h
     {
       $scope.team = data;
       console.log(data);
-      console.log($scope.team.StartTime);
-    }).error(function(err) 
-    {
-      $scope.error = err;
-    })
-  }
-  $scope.getTeamData();
-
-  $scope.changePoolNameFunc = function() {
-    $scope.changeName = !$scope.changeName;
-  }
-
-  $scope.changeNewPoolNameFunc = function(newName) {
-    $http.post($rootScope.apiUrl + "/Team/Edit", { name: newName, id: $routeParams.teamId, poolId: $routeParams.poolId, startTimes: $scope.team.StartTime, endTimes: $scope.team.EndTime})
-    .success(function(data){
-      console.log(data);
-      $scope.pool.Name = data.newName;
-      $scope.changePoolNameFunc();
-      $scope.getPoolData();
-    }).error(function(err){
-      $scope.editErr = err;
-    })
-  }
-
-  /* Change time intervals start */
-  $http.get("http://localhost:50229/Tournament/Details?id=" + $routeParams.tournamentId).success(function(data){
-    
-    if(data.status === "success"){
-    
-      $scope.tournamentId = data.Id;
-      $scope.tournamentName = data.Name;
-      $scope.tournamentPassword = data.Password;
+      console.log($scope.team.TimeIntervals);
       $scope.dateArray = [];
       $scope.startTimes = [];
       $scope.endTimes = [];
@@ -54,7 +23,9 @@ app.controller('TeamDetailController', ['$scope', '$rootScope', '$location', '$h
       $scope.endDate.setMinutes(0);
       $scope.endDate.setMilliseconds(0);
       $scope.dateRange = ($scope.endDate - $scope.startDate) / (1000 * 60 * 60 * 24);
-      
+      console.log($scope.startDate);
+      console.log($scope.endDate);
+
       for (var index = 0; index < data.TimeIntervals.length; index++) {
         var date = new Date($scope.startDate.getTime());
         date.setDate(date.getDate() + index);
@@ -62,99 +33,123 @@ app.controller('TeamDetailController', ['$scope', '$rootScope', '$location', '$h
         $scope.startTimes.push(new Date(parseInt(data.TimeIntervals[index].StartTime.substr(6))));
         $scope.endTimes.push(new Date(parseInt(data.TimeIntervals[index].EndTime.substr(6))));
       }
-      $scope.toggleMin = function () {
-        $scope.minDate = $scope.minDate ? null : new Date();
-      };
-      $scope.toggleMin();
-      $scope.maxDate = new Date(2020, 5, 22);
-      $scope.openEndDate = function ($event) {
-        $scope.statusEndDate.opened = true;
-      };
+    }).error(function(err) 
+    {
+      $scope.error = err;
+    })
+  }
+  $scope.getTeamData();
+
+  $scope.changeTeamNameFunc = function() {
+    $scope.changeName = !$scope.changeName;
+  }
+
+  $scope.changeNewPoolNameFunc = function(newName) {
+    $http.post($rootScope.apiUrl + "/Team/Edit", { name: newName, id: $routeParams.teamId, poolId: $routeParams.poolId, startTimes: $scope.team.StartTime, endTimes: $scope.team.EndTime})
+    .success(function(data){
+      console.log(data);
+      $scope.pool.Name = data.newName;
+      $scope.changeTeamNameFunc();
+      $scope.getTeamData();
+    }).error(function(err){
+      $scope.editErr = err;
+    })
+  }
+
+  /* Change time intervals start */
     
-      $scope.isChanged = function () {
-        $scope.dateRange = ($scope.endDate - $scope.startDate) / (1000 * 60 * 60 * 24);
-        $scope.dateArray = [];
-        $scope.startTimes = [];
-        $scope.endTimes = [];
-        for (var i = 0; i <= $scope.dateRange; i++) {
-          var date = new Date($scope.startDate.getTime());
-          date.setDate(date.getDate() + i);
-          $scope.dateArray.push(date);
-          $scope.startTimes.push(date);
-          $scope.endTimes.push(date);
-        }
-      }
-    
-      $scope.openStartDate = function ($event) {
-        $scope.statusStartDate.opened = true;
-      };
-      $scope.dateOptions = {
-        formatYear: 'yy',
-        startingDay: 1
-      };
-      $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
-      $scope.format = $scope.formats[0];
-      $scope.statusStartDate = {
-        opened: false
-      };
-      $scope.statusEndDate = {
-        opened: false
-      };
-      
-      $scope.uploadTournament = function () {
-        if (!$scope.tournamentName || !$scope.tournamentPassword){
-          $scope.error = "Navn eller kode ikke sat";
-        }else{
-          $scope.startDateTimes = [];
-          $scope.endDateTimes = [];
-          $scope.error = false;
-          for (var index = 0; index <= $scope.dateRange; index++) {
-            $scope.startDateTimes[index] = $scope.startTimes[index].toISOString();
-            $scope.endDateTimes[index] = $scope.endTimes[index].toISOString();        
-            
-          }
-          
-          if($scope.startDateTimes.length-1 !== $scope.dateRange && $scope.endDateTimes.length-1 !== $scope.dateRange){
-            console.log($scope.startDateTimes.length);
-            console.log($scope.dateRange);
-            $scope.error = "Fejl i start eller slut tidspunkt for en af dagene";
-              
-          }else{
-            for(var i = 0; i <= $scope.dateRange; i++){
-              if($scope.startDateTimes[i] >= $scope.endDateTimes[i]){
-                $scope.error = "alle slut tidspunkter skal være senere end start tidspunkter";
-              }
-            }
-            if(!$scope.error){
-              var tournamentData = {
-                id: $scope.tournamentId,
-                name: $scope.tournamentName,
-                password: $scope.tournamentPassword,
-                startTimes: $scope.startDateTimes,
-                endTimes: $scope.endDateTimes
-              }
-          
-              $http.post("http://localhost:50229/Tournament/Edit/", tournamentData).success(function(Data)
-              {
-                if(Data.status === "error"){
-                  $scope.error = Data.message;
-                }else{
-                  $location.path("tournament/" + Data.Id);
-                }
-              }).error(function(err) 
-              {
-                $scope.error = "Kunne ikke uploade til serveren";
-              });
-            }
-          }  
-        }   
-      }
-    }else{
-      $scope.error = "Kunne ikke finde turneringen";
+  
+  $scope.toggleMin = function () {
+    $scope.minDate = $scope.minDate ? null : new Date();
+  };
+  $scope.toggleMin();
+  $scope.maxDate = new Date(2020, 5, 22);
+  $scope.openEndDate = function ($event) {
+    $scope.statusEndDate.opened = true;
+  };
+
+  $scope.isChanged = function () {
+    $scope.dateRange = ($scope.endDate - $scope.startDate) / (1000 * 60 * 60 * 24);
+    $scope.dateArray = [];
+    $scope.startTimes = [];
+    $scope.endTimes = [];
+    for (var i = 0; i <= $scope.dateRange; i++) {
+      var date = new Date($scope.startDate.getTime());
+      date.setDate(date.getDate() + i);
+      $scope.dateArray.push(date);
+      $scope.startTimes.push(date);
+      $scope.endTimes.push(date);
     }
-  }).error(function(err){
-    $scope.error = "Kunne ikke finde turneringen";
-  });
+  }
+
+  $scope.openStartDate = function ($event) {
+    $scope.statusStartDate.opened = true;
+  };
+  $scope.dateOptions = {
+    formatYear: 'yy',
+    startingDay: 1
+  };
+  $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+  $scope.format = $scope.formats[0];
+  $scope.statusStartDate = {
+    opened: false
+  };
+  $scope.statusEndDate = {
+    opened: false
+  };
+  
+  $scope.uploadTeam = function () {
+    if (!$scope.team.Name){
+      $scope.error = "Navn er ikke sat";
+    }else{
+      $scope.startDateTimes = [];
+      $scope.endDateTimes = [];
+      $scope.error = false;
+      console.log($scope.dateRange);
+      console.log($scope.startTimes);
+      for (var index = 0; index <= $scope.startTimes.length; index++) {
+        $scope.startDateTimes[index] = $scope.startTimes[index].toISOString();
+        $scope.endDateTimes[index] = $scope.endTimes[index].toISOString();        
+        
+      }
+      
+      console.log($scope.startDateTimes);
+
+      if($scope.startDateTimes.length-1 !== $scope.dateRange && $scope.endDateTimes.length-1 !== $scope.dateRange){
+        console.log($scope.startDateTimes.length);
+        console.log($scope.dateRange);
+        $scope.error = "Fejl i start eller slut tidspunkt for en af dagene";
+          
+      }else{
+        for(var i = 0; i <= $scope.dateRange; i++){
+          if($scope.startDateTimes[i] >= $scope.endDateTimes[i]){
+            $scope.error = "alle slut tidspunkter skal være senere end start tidspunkter";
+          }
+        }
+        if(!$scope.error){
+          var teamData = {
+            id: $scope.team.Id,
+            name: $scope.team.Name,
+            poolId: $routeParams.poolId,
+            startTimes: $scope.startDateTimes,
+            endTimes: $scope.endDateTimes
+          }
+      
+          $http.post("http://localhost:50229/Team/Edit/", teamData).success(function(Data)
+          {
+            console.log(Data);
+            if(Data.status === "error"){
+              $scope.error = Data.message;
+            }
+          }).error(function(err)
+          {
+            $scope.error = err;
+          });
+          }
+        }  
+      }   
+  
+  }
 
 
   /* Change time intervals end */ 
