a94ac46601ed823ce82455022f086c2f413bab8a
bkhoja <bilalkhwaja2694@gmail.com>
2015-11-18 09:56:13 +0100

    finals link controller added

---
+++ CupPlaner/CupPlanerTests/Controllers/FieldControllerTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CupPlaner.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CupPlaner.Controllers.Tests
+{
+    [TestClass()]
+    public class FieldControllerTests
+    {
+        [TestMethod()]
+        public void CreateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAllTournamentFieldsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DetailsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
