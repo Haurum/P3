e5716cae9d5e7ee0134a4c3a1b5333c2dd670ec3
bovle <frederik.bl@live.dk>
2015-12-08 12:37:33 +0100

    generate

--- CupPlaner/CupPlaner/Controllers/MatchGenerationController.cs
+++ CupPlaner/CupPlaner/Controllers/MatchGenerationController.cs
@@ -3,20 +3,69 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CupPlaner.Helpers;
 
 namespace CupPlaner.Controllers
 {
     public class MatchGenerationController : Controller
     {
-        // GET: MatchGeneration
-        public ActionResult Index()
-        {
-            return View();
-        }
+        MatchGeneration mg = new MatchGeneration();
 
         public ActionResult GenerateGroupStage(int tournamentID)
         {
+            try
+            {
+                if (mg.GenerateGroupStage(tournamentID))
+                {
+                    return Json(new { status = "success" });
+                }
+                else
+                {
+                    return Json(new { status = "error", message = "Generering mislykkedes" });
+                }
+            }
+            catch (Exception)
+            {
+                return Json(new { status = "error", message = "fejl i programmet" });
+            }
+        }
 
+        public ActionResult GenerateFinalsTeams(int tournamentID)
+        {
+            try
+            {
+                if (mg.GenerateFinalsTeams(tournamentID))
+                {
+                    return Json(new { status = "success" });
+                }
+                else
+                {
+                    return Json(new { status = "error", message = "Generering mislykkedes" });
+                }
+            }
+            catch (Exception)
+            {
+                return Json(new { status = "error", message = "fejl i programmet" });
+            }
+        }
+
+        public ActionResult GenerateFinalsMatches(int tournamentID)
+        {
+            try
+            {
+                if (mg.GenerateFinalsMatches(tournamentID))
+                {
+                    return Json(new { status = "success" });
+                }
+                else
+                {
+                    return Json(new { status = "error", message = "Generering mislykkedes" });
+                }
+            }
+            catch (Exception)
+            {
+                return Json(new { status = "error", message = "fejl i programmet" });
+            }
         }
     }
 }
